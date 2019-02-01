7b391c84dbda1ab963ebec769cabd4f9db2a8e00
Thomas Watson <twatson52@icloud.com>
2019-02-01 11:46:55 -0600

    implement full server with the connector class

--- mbus/server.py
+++ mbus/server.py
@@ -2,177 +2,137 @@
 
 import asyncio
 import os
-import struct
-import pickle
+import traceback
 
-# handle a subscriber
-class SubscriberHandler:
-    def __init__(self, server, reader, writer):
-        self.server = server
-        self.reader = reader
-        self.writer = writer
-        self.sendq = asyncio.Queue()
-        self.closing = False
+from enum import Enum
 
-        # initialize read buffer
-        self.read_blocks = []
-        self.read_bytes = 0
+from connector import ConnectionEndedError, BusConnector
 
-        # start task to receive data from client
-        self.rx_task = asyncio.ensure_future(self.receive_data())
-        # and then task to send data from the queue
-        self.tx_task = asyncio.ensure_future(self.send_data())
-
-    async def read_exactly(self, n):
-        # read exactly n bytes of data from the reader
-
-        while self.read_bytes < n:
-            # read in some new data
-            data = await self.reader.read(65536)
-            if len(data) == 0: # reader disconnected
-                # since we are still in the loop and received no new data,
-                # we will never receive enough data to satisfy the request
-                # so die now
-                raise ConnectionResetError()
-            # add it to the read buffer as its own entity
-            self.read_blocks.append(data)
-            # and note how many bytes are now in the buffer
-            self.read_bytes += len(data)
-
-        # we have enough, so join all the blocks together into one big piece
-        data = b''.join(self.read_blocks)
-        # and split it into what the user wants and what's left over for later
-        out = data[:n]
-        remaining = data[n:]
-        self.read_blocks = [remaining]
-        self.read_bytes = len(remaining)
-
-        return out
-
-    def die(self):
-        # die because the receive or send loops have ended
-        # schedule a task to close them out
-        if self.closing is False:
-            self.closing = True
-            asyncio.ensure_future(self.close())
-
-    async def close(self):
-        print("closing", self)
-        # we could be in any state
-        self.closing = True
-
-        # tell the server to clean us up and stop sending us messages
-        self.server.close_subscriber(self)
-
-        # ask the reader and writer tasks to cancel
-        if not self.rx_task.done():
-            self.rx_task.cancel()
-        if not self.tx_task.done():
-            self.tx_task.cancel()
-
-        # wait for them to cancel and clean up
-        try:
-            await self.rx_task
-        except:
-            pass
-        try:
-            await self.tx_task
-        except:
-            pass
-
-        # finally, close off the writer socket
-        self.writer.close()
-
-    async def receive_data(self):
-        # loop to receive data from the client and forward it on to the server
-
-        try:
-            while True:
-                # get data length
-                dlen_bytes = await self.read_exactly(4)
-                dlen, = struct.unpack("<I", dlen_bytes)
-
-                # and then that much data
-                data = await self.read_exactly(dlen)
-
-                msg = pickle.loads(data)
-
-                self.server.route(self.sendq, msg)
-        except asyncio.CancelledError:
-            # let ourselves be cancelled naturally
-            raise
-        except:
-            # otherwise, just end the connection now
-            self.die()
-
-    async def send_data(self):
-        # loop to send data from the server and forward it to the client
-
-        try:
-            while True:
-                # get a new message
-                msg = await self.sendq.get()
-
-                data = pickle.dumps(msg)
-
-                self.writer.write(struct.pack("<I", len(data)))
-                self.writer.write(data)
-
-                await self.writer.drain()
-        except asyncio.CancelledError:
-            # let ourselves be cancelled naturally
-            raise
-        except:
-            # otherwise, just end the connection now
-            self.die()
-
+# things to do with the tag in the metadata
+class MessageAction(Enum):
+    SUBSCRIBE = 1
+    UNSUBSCRIBE = 2
+    SEND = 3
 
 class MessageBusServer:
     def __init__(self, bus_addr):
         self.bus_addr = bus_addr
-        self.subscribers_for_tag = {}
-        self.tags_for_subscriber = {}
+        self.connectors_for_tag = {}
+        self.connectors = set()
 
     async def serve(self):
         self.server = await asyncio.start_unix_server(self.subscriber_connected, 
             path=self.bus_addr)
 
     def subscriber_connected(self, reader, writer):
-        # instantiate a handler for this subscriber
-        subscriber = SubscriberHandler(self, reader, writer)
-        # it will call us back when it receives a messages
-        # remember it so we can tell it to stop
-        self.tags_for_subscriber[subscriber] = set()
+        # instantiate a connector to manage this subscriber
+        connector = BusConnector(reader, writer)
+        # create a task to receive messages from this connector
+        asyncio.ensure_future(self.rx_messages(connector))
+        # it will die when the connector dies, so we don't need to
+        # keep it around
+
+        self.connectors.add(connector)
 
     async def close(self):
         # close off the server so we don't get any new connections
         self.server.close()
         await self.server.wait_closed()
 
+        connectors = self.connectors
+
         # forget all the subscriptions so we don't waste time searching them
-        subscribers = self.tags_for_subscriber.keys()
-        self.tags_for_subscriber = None
-        self.subscribers_for_tag = None
+        # or unexpectedly route messages while half-closed
+        self.connectors_for_tag = None
+        self.connectors = None
 
-        # close the connection to each subscriber
-        for subscriber in subscribers:
-            await subscriber.close()
+        # close all our connectors
+        for connector in connectors:
+            await connector.close()
+            # closing the connector will cause it to raise
+            # a ConnectionEndedError in recv()
+            # which will stop the rx task
 
-    def close_subscriber(self, subscriber):
-        # forget this subscriber entirely
+    def forget_connector(self, connector):
+        # forget this connector entirely
 
         # busy shutting down
-        if self.tags_for_subscriber is None:
+        if self.connectors is None:
             return
 
-        tags = self.tags_for_subscriber[subscriber]
-        del self.tags_for_subscriber[subscriber]
+        # if this is called, the connector has already closed itself
+        # so all we need to do is forget that it existed
 
-        # remove all subscriptions to the tags this subscriber has
-        # so that we don't try to send it any more messages
-        for tag in tags:
-            del self.subscribers_for_tag[tag][subscriber]
+        try:
+            self.connectors.remove(connector)
+        except KeyError:
+            # this connector doesn't exist anymore
+            # so don't bother removing it from connectors_for_tag
+            return
 
+        # find and remove this connector from any tags
+        for tag_connectors in self.connectors_for_tag.values():
+            try:
+                tag_connectors.remove(connector)
+            except KeyError:
+                pass
+
+    async def rx_messages(self, connector):
+        # task to receive and process messages from a particular connector
+        try:
+            while True:
+                meta, data = await connector.recv()
+                self.route(connector, meta, data)
+        except ConnectionEndedError:
+            # expected cause of death
+            pass
+        except Exception as e:
+            # just print the exception so this task can die in peace
+            traceback.print_exc()
+            # and make sure the connector is closed, no matter the error
+            await connector.close()
+        finally:
+            # make sure this connector is forgotten
+            # this may double forget it, but that's safe
+            self.forget_connector(connector)
+
+    def route(self, connector, meta, data):
+        # the first element of meta is an action to do with the tag
+        # and the second is the tag as a string
+
+        # we don't care what's in the data bytes
+        action, tag = meta
+
+        if mtype == MessageAction.SEND:
+            # figure out who is interested in this message
+            c_set = self.connectors_for_tag.get(tag)
+            if c_set is None:
+                # nobody, apparently
+                return
+            # and send the message to all of em
+            # copy the set so if we need to forget a connection we can do it
+            # without changing the set we're iterating over
+            for connector in c_set.copy():
+                # the metadata is just the tag name
+                try:
+                    connector.send(tag, data)
+                except ConnectionEndedError:
+                    # oops, the connector is closed
+                    self.forget_connector(connector)
+        elif mtype == MessageAction.SUBSCRIBE:
+            # add this connector to the set of connectors for this tag
+            # so that it will receive future messages
+            c_set = self.connectors_for_tag.get(tag)
+            if c_set is None:
+                # nobody's subscribed to this tag yet, so create a set
+                c_set = set()
+                self.connectors_for_tag[tag] = c_set
+            c_set.add(connector)
+        elif mtype == MessageAction.UNSUBSCRIBE:
+            try:
+                self.connectors_for_tag[tag].remove(connector)
+            except KeyError:
+                pass
 
 
 if __name__ == "__main__":
