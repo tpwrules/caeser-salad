602e9c96108230381f8685994b6e21338b851e6c
Thomas Watson <twatson52@icloud.com>
2019-02-03 14:40:57 -0600

    revamp closing logic in client

--- mbus/client.py
+++ mbus/client.py
@@ -6,6 +6,23 @@ import traceback
 
 from connector import ConnectionClosedError, BusConnector, MessageAction
 
+# how we close down
+# closing can start from various sources
+# 1a. close() is called
+# 1b. _handle_rx is is called with None cause the connector closed
+# then we begin the process
+# 2. _start_close() is called
+# 3. if not already created, start_close creates a task which runs
+#    _do_close(). this locks out all other functions. if it is already created,
+#    _start_close() returns without doing anything
+# 4. _do_close() begins running and flushes the connector
+# 5. _do_close() closes the connector and waits for it to be closed
+# 6. _do_close() calls all the callbacks and stuffs all the queues to let them
+#    all know we are closed
+# 7. _do_close() cleans up memory we don't need anymore
+# 8. _do_close() task finished
+# 9. close() awaits on the close task, and closing is finished
+
 class MessageBusClient:
     @classmethod
     async def create(cls, bus_addr):
@@ -26,21 +43,23 @@ class MessageBusClient:
         # keep refcount of our subscriptions
         self._subscriptions = {}
 
-        self._closed = False
+        # this task is created to close down everything
+        # if it is not None, then functions raise ConnectionClosedError()
+        # or otherwise act as if the connection is closed
+        self._close_task = None
 
     def _handle_rx(self, connector, meta, data):
         # the connector is calling us with a new message
         if meta is None and data is None:
             # the connector died and is letting us know
-            # we did close
-            self._closed = True
-            asyncio.ensure_future(self.close())
-            return
-
-        try:
-            self._process(meta, data)
-        except Exception as e:
-            traceback.print_exc()
+            # we need to close ourselves in response
+            self._start_close()
+        else:
+            try:
+                # the meta is just the tag on reception
+                self._process(meta, data)
+            except Exception as e:
+                traceback.print_exc()
 
     def _process(self, tag, data):
         try:
@@ -48,14 +67,14 @@ class MessageBusClient:
             message = pickle.loads(data)
         except:
             print("MESSAGE UNPICKLE FAILURE")
-            print(meta, data)
+            print(tag, data)
             traceback.print_exc()
 
         print("got a message", message)
 
     def send(self, tag, message):
-        if self._closed:
-            raise ConnectionClosedError()
+        if self._close_task is not None:
+            raise ConnectionClosedError("can't send: connection is closed")
 
         if not isinstance(tag, str):
             raise ValueError("tag must be string")
@@ -65,8 +84,8 @@ class MessageBusClient:
         self._connector.send((MessageAction.SEND, tag), mdata)
 
     def subscribe(self, tag):
-        if self._closed:
-            raise ConnectionClosedError()
+        if self._close_task is not None:
+            raise ConnectionClosedError("can't subscribe: connection is closed")
 
         if not isinstance(tag, str):
             raise ValueError("tag must be string")
@@ -76,7 +95,7 @@ class MessageBusClient:
         self._subscriptions[tag] = subscriptions+1
 
     def unsubscribe(self, tag):
-        if self._closed:
+        if self._close_task is not None:
             return
 
         if not isinstance(tag, str):
@@ -90,16 +109,30 @@ class MessageBusClient:
         if subscriptions-1 == 0:
             self._connector.send((MessageAction.UNSUBSCRIBE, tag), b'')
 
-    async def close(self):
-        # we are done
-        self._closed = True
 
+    def _start_close(self):
+        if self._close_task is None:
+            # schedule a task to do the actual closing
+            self._close_task = asyncio.create_task(self._do_close())
+
+    async def _do_close(self):
         # close the connector to be sure we don't receive or send anything more
         # but wait for curent messages to be sent
         await self._connector.flush()
         await self._connector.close()
 
         # for now we don't have to tell anybody else that we are closing
+
+        # clean up things we won't need anymore
+        # probably not exactly necessary
+        self._connector = None
+        self._subscriptions = None
+
+    async def close(self):
+        # start the close process (if it's not already started)
+        self._start_close()
+        # and wait for it to finish
+        await self._close_task
 
 
 async def main():
