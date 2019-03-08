8111bb16acd08101c11a5cc3bd0b0a2345bd2915
Thomas Watson <twatson52@icloud.com>
2019-03-07 19:07:59 -0600

    download vehicle parameters test

--- mavstuff/mavdump.py
+++ mavstuff/mavdump.py
@@ -3,15 +3,36 @@
 import importlib
 import socket
 
-mavlink = importlib.import_module("pymavlink.dialects.v20.ardupilotmega")
+class WProxy:
+    def __init__(self, conn):
+        self.conn = conn
 
-mav = mavlink.MAVLink(None, srcSystem=254, srcComponent=195, use_native=True)
+    def write(self, data):
+        print(self.conn.send(data))
+
+mavlink = importlib.import_module("pymavlink.dialects.v10.ardupilotmega")
+
 
 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-conn.connect(("localhost", 5763))
+conn.connect(("localhost", 5760))
 
+mav = mavlink.MAVLink(WProxy(conn), 
+    srcSystem=254, srcComponent=195, use_native=True)
+
+import time
+
+
+
+mi = 0
 while True:
     b = conn.recv(mav.bytes_needed())
     msg = mav.parse_char(b)
     if msg is not None:
-        print(msg)
+        print(msg, msg.get_srcSystem(), msg.get_srcComponent())
+        mi += 1
+        if mi > 5:
+            mav.heartbeat_send(mavlink.MAV_TYPE_ONBOARD_CONTROLLER, 0, 0, 0, 
+                mavlink.MAV_STATE_ACTIVE, 3)
+        if mi == 7:
+            mav.param_request_list_send(1, 1)
+
