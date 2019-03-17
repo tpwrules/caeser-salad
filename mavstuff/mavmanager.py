0b0599536ca25acd2ab21d8ef1663d30d58b0d70
Thomas Watson <twatson52@icloud.com>
2019-03-17 13:12:34 -0500

    stuff idk quite

--- mavstuff/mavmanager.py
+++ mavstuff/mavmanager.py
@@ -4,16 +4,30 @@ import asyncio
 
 import router
 import destination
+import route_mbus
 
 async def main():
-    # we want to connect to the actual drone first
-    reader, writer = await asyncio.open_connection('localhost', 5763)
-
-    # crappily print messages from it
     try:
-        dest = destination.StreamDestination(reader, writer)
+        # first, start up the router in its own task
+        router = router.Router()
+        route_task = asyncio.create_task(router.route())
+
+        # now connect to the message bus
+        mbus_manager = \
+            mav_mbus.MBusDestinationManager("./socket_mbus_main", router)
+        # and start the manager on its own task too
+        manage_task = asyncio.create_task(mbus_manager.manage())
+
+        # now that we are conneced to the rest of the system, 
+        # connect to the drone
+        reader, writer = await asyncio.open_connection('localhost', 5763)
+        # it is its own destination, so create one for it
+        drone_dest = destination.StreamDestination(reader, writer)
+        router.add_destination(drone_dest)
+
+        # now everything will behave itself and we can just snooze
         while True:
-            msg, src = await dest.read_msg()
+            await asyncio.sleep(1)
     finally:
         # theoretically, close things out
         # i don't think much needs to be done here?
---
+++ mavstuff/route_mbus.py
@@ -0,0 +1,7 @@
+# this file manages all the message bus connections for the MAVlink router
+
+import asyncio
+
+import destination
+
+from ..mbus import client as mclient
