4e727cefc6783a03d7873a6c0e0354daaf53fc34
Thomas Watson <twatson52@icloud.com>
2019-03-17 12:11:35 -0500

    implement mav destination concept and crappy router

---
+++ mavstuff/destination.py
@@ -0,0 +1,117 @@
+# this class encompasses a mavlink Destination
+
+import pymavlink.dialects.v20.ardupilotmega as mavlink
+
+class Destination:
+    def __init__(self):
+        # set of system ids we send to
+        self._sysids = set()
+        # set of (system, component) ids we send to
+        self._compids = set()
+
+
+    # read one message from the destination's transport
+    async def _get_msg(self):
+        raise NotImplementedError
+
+    # write one message to the destination's transport
+    def _put_msg(self, msg, src, dest):
+        raise NotImplementedError
+
+
+    async def read_msg(self):
+        msg, src = await self._get_msg()
+
+        # for now, don't bother parsing the message
+        print(msg, type(msg))
+
+        return msg, src
+
+    # write one message, if this destination should send it
+    # if we sent it, return True
+    def write_msg(self, msg, src, dest):
+        if dest[0] == 0:
+            should_send = True
+        elif dest[0] in self._sysids and dest[1] == 0:
+            should_send = True
+        elif dest in self._compids:
+            should_send = True
+
+        if should_send:
+            self._put_msg(msg, src, dest)
+
+        return should_send
+
+# a destination that needs byte encoding/decoding
+class BytesDestination(Destination):
+    # Source object simply has source IDs and a sequence number for that ID
+    class Source:
+        def __init__(self, src):
+            self.srcSystem = src[0]
+            self.srcComponent = src[1]
+            self.seq = 0
+
+        def pack(self, msg):
+            # tell the message to get packed with ourselves as the mav state
+            packed = msg.pack(self)
+            # sequence id is 8 bit
+            self.seq = (self.seq + 1) & 0xFF
+            return packed
+
+    def __init__(self):
+        super().__init__()
+
+        # we need Sources for each source we send
+        # so we can pack messages correctly and keep track of sequence numbers
+        # dictionary of (src_sysid, src_compid) to Source objects
+        self._sources = {}
+
+        # we also need a proper mavlink object so we can receive and decode
+        # messages from the transport
+        # but it doesn't have a file cause we don't send things with it
+        self._mav = mavlink.MAVLink(None)
+
+
+    # read at least one byte from the transport
+    async def _read(self):
+        raise NotImplementedError
+
+    # write some bytes to the transport
+    def _write(self, data):
+        raise NotImplementedError
+
+
+    async def _get_msg(self):
+        # make sure we send out any messages already in the buffer first
+        # so we don't unnecessarily wait for more bytes
+        msg = self._mav.parse_char(b'')
+        while msg is None:
+            # receive some bytes from the transport
+            new_bytes = await self._read()
+            # and try to parse a message from them
+            msg = self._mav.parse_char(new_bytes)
+            # if None, there weren't enough bytes
+
+        return msg, (msg.get_srcSystem(), msg.get_srcComponent())
+
+    def _put_msg(self, msg, src, dest):
+        source = self._sources.get(src)
+        if source is None:
+            source = Source(src)
+            self._sources[src] = source
+
+        self._write(source.pack(msg))
+
+# destination based on an asyncio reader, writer pair
+class StreamDestination(BytesDestination):
+    def __init__(self, reader, writer):
+        super().__init__()
+
+        self._reader = reader
+        self._writer = writer
+
+    async def _read(self):
+        return await self._reader.read(100)
+
+    def _write(self, data):
+        return self._writer.write(data)
---
+++ mavstuff/mavrouter.py
@@ -0,0 +1,23 @@
+import asyncio
+
+import pymavlink.dialects.v20.ardupilotmega as mavlink
+
+import destination
+
+
+async def main():
+    # we want to connect to the actual drone first
+    reader, writer = await asyncio.open_connection('localhost', 5763)
+
+    # crappily print messages from it
+    try:
+        dest = destination.StreamDestination(reader, writer)
+        while True:
+            msg, src = await dest.read_msg()
+    finally:
+        # theoretically, close things out
+        # i don't think much needs to be done here?
+        pass
+
+if __name__ == "__main__":
+    asyncio.run(main())
