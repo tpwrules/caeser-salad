1edb4bec9bc9a6cabe09fc6558ed53fdf0e6e8eb
Thomas Watson <twatson52@icloud.com>
2019-03-19 12:42:33 -0500

    implement message filters

--- caeser_salad/mavstuff/mbus_component.py
+++ caeser_salad/mavstuff/mbus_component.py
@@ -10,6 +10,14 @@ import pymavlink.dialects.v20.ardupilotmega as mavlink
 from caeser_salad.mbus import client as mclient
 from caeser_salad.mavstuff.mbus_messages import *
 
+class ComponentMessageFilter:
+    def __init__(self, filter_q):
+        # queue of filtered messages
+        self._filter_q = filter_q
+
+    async def next_message(self):
+        return await self._filter_q.get()
+
 class MBusComponent:
     def __init__(self, mbus, tag, sysid, compid):
         # create a filter for mavlink messages on the given tag
@@ -33,6 +41,33 @@ class MBusComponent:
         self._rx_task = asyncio.create_task(self._rx_messages())
 
 
+    def create_message_filter(self, msgs=None):
+        # if not None, messages is an iterable of mavlink messages to filter for
+        # i.e. they are subclasses of mavlink.MAVLink_message
+
+        msgs_to_filter = None
+        if msgs is not None:
+            msgs_to_filter = set()
+            for candidate in msgs:
+                if not issubclass(candidate, mavlink.MAVLink_message):
+                    raise ValueError(("{} is not a subclass of "+
+                        "mavlink.MAVLink_message").format(candidate))
+                msgs_to_filter.add(candidate)
+
+        filter_q = asyncio.Queue()
+        if msgs_to_filter is not None:
+            for msg_to_filter in msgs_to_filter:
+                filters_on_msg = self._message_filters.get(msg_to_filter)
+                if filters_on_msg is None:
+                    filters_on_msg = set()
+                    self._message_filters[msg_to_filter] = filters_on_msg
+                filters_on_msg.add(filter_q)
+        else:
+            self._message_pipes.add(filter_q)
+
+        return ComponentMessageFilter(filter_q)
+
+
     async def _send_heartbeat(self):
         # automatically send the heartbeat message every second
         # to do: send current state
@@ -49,7 +84,6 @@ class MBusComponent:
         # filter queues
         while True:
             tag, msg = await self._mfilter.recv()
-            print("rxd", msg)
 
             queues = self._message_filters.get(type(msg.msg))
             if queues is not None:
@@ -72,8 +106,10 @@ async def main():
 
     comp = MBusComponent(mbus, "mav_test", 1, 195)
 
+    f = comp.create_message_filter((mavlink.MAVLink_heartbeat_message,))
+
     while True:
-        await asyncio.sleep(1)
+        print(await f.next_message())
 
 if __name__ == "__main__":
     asyncio.run(main())
