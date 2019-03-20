0e2ccbc28654eb0f664f2ef5c74f0114f676c485
Thomas Watson <twatson52@icloud.com>
2019-03-20 12:29:15 -0500

    implement component command handler

--- caeser_salad/mavstuff/mbus_component.py
+++ caeser_salad/mavstuff/mbus_component.py
@@ -18,6 +18,29 @@ class ComponentMessageFilter:
     async def next_message(self):
         return await self._filter_q.get()
 
+class ComponentCommandHandler:
+    def __init__(self, cmd_q, component):
+        self._cmd_q = cmd_q
+        self._component = component
+        self._last_cmd = None
+
+    async def next_command(self):
+        # todo: figure out how to handle confirmation number
+        if self._last_cmd is not None:
+            raise RuntimeError("last command was never responded to")
+        self._last_cmd = await self._cmd_q.get()
+        return self._last_cmd
+
+    def respond(self, result):
+        if result not in mavlink.enums["MAV_RESULT"]:
+            raise ValueError("{} is not a MAV_RESULT".format(result))
+        if self._last_cmd is None:
+            raise RuntimeError("last command was already responded to")
+        self._component._send_msg(mavlink.MAVLink_command_ack_message(
+            self._last_cmd.msg.command, result))
+        self._last_cmd = None
+
+
 class MBusComponent:
     def __init__(self, mbus, tag, sysid, compid):
         # create a filter for mavlink messages on the given tag
@@ -28,11 +51,16 @@ class MBusComponent:
         self._tag = tag
         self._src = (sysid, compid)
 
-        # dictionary of message to set of filters. you can have many filters per
-        # message
+        # dictionary of message to set of queues that go to filters.
+        # you can have many filters per message
         self._message_filters = {}
         # stores filterless filters that receive all messages
         self._message_pipes = set()
+
+        # dictionary of command ID (in the MAV_CMD enum) to queues that go to
+        # command handlers
+        # you can only have one command handler per command
+        self._command_handlers = {}
 
         # start sending heartbeats from this component
         self._heartbeat_task = asyncio.create_task(self._send_heartbeat())
@@ -68,6 +96,26 @@ class MBusComponent:
         return ComponentMessageFilter(filter_q)
 
 
+    def create_command_handler(self, cmds=None):
+        # cmds is an iterable of mavlink commands to filter for
+        # they must be in the MAV_CMD enum
+
+        cmds_to_filter = set()
+        for cmd in cmds:
+            # make sure this is a valid command number
+            if cmd not in mavlink.enums["MAV_CMD"]:
+                raise ValueError("{} is not a MAV_CMD".format(cmd))
+            # make sure there isn't a handler for this command already
+            if cmd in self._command_handlers:
+                raise ValueError("MAV_CMD {} already has a handler".format(cmd))
+            cmds_to_filter.add(cmd)
+
+        cmd_q = asyncio.Queue()
+        for cmd in cmds_to_filter:
+            self._command_handlers[cmd] = cmd_q
+
+        return ComponentCommandHandler(cmd_q, self)
+
     async def _send_heartbeat(self):
         # automatically send the heartbeat message every second
         # to do: send current state
@@ -85,12 +133,19 @@ class MBusComponent:
         while True:
             tag, msg = await self._mfilter.recv()
 
-            queues = self._message_filters.get(type(msg.msg))
-            if queues is not None:
-                for queue in queues:
+            # this message is a command to do something
+            if isinstance(msg.msg, mavlink.MAVLink_command_long_message):
+                # so tell the appropriate command handler
+                handler_q = self._command_handlers.get(msg.msg.command)
+                if handler_q is not None:
+                    handler_q.put_nowait(msg)
+            else: # it's just a normal message. tell message filters
+                queues = self._message_filters.get(type(msg.msg))
+                if queues is not None:
+                    for queue in queues:
+                        queue.put_nowait(msg)
+                for queue in self._message_pipes:
                     queue.put_nowait(msg)
-            for queue in self._message_pipes:
-                queue.put_nowait(msg)
 
 
     def _send_msg(self, msg):
@@ -106,10 +161,12 @@ async def main():
 
     comp = MBusComponent(mbus, "mav_test", 1, 195)
 
-    f = comp.create_message_filter((mavlink.MAVLink_heartbeat_message,))
+    c = comp.create_command_handler((
+        mavlink.MAV_CMD_REQUEST_CAMERA_INFORMATION,))
 
     while True:
-        print(await f.next_message())
+        print(await c.next_command())
+        c.respond(mavlink.MAV_RESULT_UNSUPPORTED)
 
 if __name__ == "__main__":
     asyncio.run(main())
